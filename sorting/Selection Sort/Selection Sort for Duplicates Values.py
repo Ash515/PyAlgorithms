4248d2963c76512985030eae6c3dec4cd5b9ca34
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 <aaashwin515@gmail.com>
2021-08-02 14:53:09 +0530

    Added Selection Sort

--- sorting/Selection Sort/Selection Sort for Duplicates Values.py
+++ sorting/Selection Sort/Selection Sort for Duplicates Values.py
@@ -1,6 +1,6 @@
 #Acending Order
 list1=[56,3,2,78,6,0]
-print(list1)
+print("Unsorted list",list1)
 for i in range(len(list1)):
     min_val=min(list1[i:])
     min_ind=list1.index(min_val,i)
@@ -10,7 +10,7 @@ print(list1)  #[0, 2, 3, 6, 56, 78]
 #Descending Order
 
 list1=[56,3,2,78,6,0]
-print(list1)
+print("Unsorted list",list1)
 for i in range(len(list1)):
     min_val=max(list1[i:])
     min_ind=list1.index(min_val,i)
