c7696fb55c399e541c30207060a0d18f2591e852
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 <aaashwin515@gmail.com>
2021-08-05 21:17:55 +0530

    added new files

--- sorting/Bubblesort.py
+++ sorting/Bubblesort.py
@@ -1,14 +1,9 @@
-def bubblesort(list):
+arr=[10,15,4,23,0]
+for i in range(len(arr)-1):
+    for j in range(len(arr)-1):
+        if (arr[j]>arr[j+1]):
+            arr[j],arr[j+1]=arr[j+1],arr[j]
+print(arr)
 
-# Swap the elements to arrange in order
-    for iter_num in range(len(list)-1,0,-1):
-        for idx in range(iter_num):
-            if list[idx]>list[idx+1]:
-                temp = list[idx]
-                list[idx] = list[idx+1]
-                list[idx+1] = temp
+#[0, 4, 10, 15, 23]
 
-
-list = [19,2,31,45,6,11,121,27]
-bubblesort(list)
-print(list)
