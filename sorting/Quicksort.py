5abbebbc1fabf9fc7c18ac0daaf53bdb9d94251d
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 <aaashwin515@gmail.com>
2021-08-07 21:46:45 +0530

    added quick sort

--- sorting/Quicksort.py
+++ sorting/Quicksort.py
@@ -1,42 +1,73 @@
-def partition(arr,low,high): 
-    i = ( low-1 )         # index of smaller element 
-    pivot = arr[high]     # pivot 
-  
-    for j in range(low , high): 
-  
-        # If current element is smaller than the pivot 
-        if   arr[j] < pivot: 
-          
-            # increment index of smaller element 
-            i = i+1 
-            arr[i],arr[j] = arr[j],arr[i] 
-  
-    arr[i+1],arr[high] = arr[high],arr[i+1] 
-    return ( i+1 ) 
-  
-# The main function that implements QuickSort 
-# arr[] --> Array to be sorted, 
-# low  --> Starting index, 
-# high  --> Ending index 
-  
-# Function to do Quick sort 
-def quickSort(arr,low,high): 
-    if low < high: 
-  
-        # pi is partitioning index, arr[p] is now 
-        # at right place 
-        pi = partition(arr,low,high) 
-  
-        # Separately sort elements before 
-        # partition and after partition 
-        quickSort(arr, low, pi-1) 
-        quickSort(arr, pi+1, high) 
-  
-# Driver code to test above 
-arr = [10, 7, 8, 9, 1, 5] 
-n = len(arr) 
-quickSort(arr,0,n-1) 
-print ("Sorted array is:") 
-for i in range(n): 
-    print ("%d" %arr[i]), 
-  
+def pivot_place(list1,first,last):
+    pivot=list1[first]
+    left=first
+    right=last
+    while True:
+        while left<=right and list1[left]<=pivot:
+            left+=1
+        while left<=right and list1[right]>=pivot:
+            right-=1
+        
+        if left>right:
+            break
+        else:
+            list1[left],list1[right]=list1[right],list1[left]
+    list1[first],list1[right]=list1[right],list1[first]
+    return right
+    
+
+def quicksort(list1,first,last):
+    if first<last:
+        p=pivot_place(list1,first,last)
+        quicksort(list1,first,p-1)
+        quicksort(list1,p+1,last)
+
+#driver code
+array=[20,30,10,50,70,100]
+quicksort(array,0,len(array)-1)
+
+print(array)  #[10, 20, 30, 50, 70, 100]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
