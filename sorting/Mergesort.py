5c93f08ebf8ff2b7a704dff2d417bba03192309c
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 <aaashwin515@gmail.com>
2021-08-09 13:56:16 +0530

    added merge sort

--- sorting/Mergesort.py
+++ sorting/Mergesort.py
@@ -1,3 +1,53 @@
+def mergesort(list1):
+    if len(list1)>1:
+        mid = len(list1)//2
+        left_list=list1[:mid]
+        right_list=list1[mid:]
+        mergesort(left_list)
+        mergesort(right_list)
+
+        i=0
+        j=0
+        k=0
+        while i<len(left_list) and j<len(right_list):
+            if left_list[i]<right_list[j]:
+                list1[k]=left_list[i]
+                i+=1
+                k+=1
+            else:
+                list1[k]=right_list[j]
+                j+=1
+                k+=1
+        while i<len(left_list):
+            list1[k]=left_list[i]
+            i+=1
+            k+=1
+        while j<len(right_list):
+            list1[k]=right_list[j]
+            j+=1
+            k+=1
+        
+
+l=[10,20,5,30,15]
+mergesort(l)
+print(l)   #[5, 10, 15, 20, 30]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
 def merge_sort(unsorted_list):
     if len(unsorted_list) <= 1:
         return unsorted_list
@@ -31,3 +81,4 @@ def merge(left_half,right_half):
 unsorted_list = [64, 34, 25, 12, 22, 11, 90]
 
 print(merge_sort(unsorted_list))
+'''
